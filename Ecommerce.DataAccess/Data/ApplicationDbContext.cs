d1fc3bb94333e82bc5ea2b37a4ef831bef712b2f
Samip Khanal <itssamip@gmail.com>
2021-07-27 12:42:59 +0545

    Add project files.

---
+++ Ecommerce.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Ecommerce.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ecommerce.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Menu> Menus { get; set; }
+
+    }
+}
---
+++ Ecommerce.Models/Menu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Models
+{
+    public class Menu
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        [Display(Name ="Menu Names")]
+        [Required]
+        public string Name { get; set; }
+    }
+}
